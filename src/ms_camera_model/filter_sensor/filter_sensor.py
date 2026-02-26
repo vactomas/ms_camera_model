50c91a1bd355e9c2a8e4c44960c4f4f2393c42dd
Tomas Vacek <vacek.tomas@weaport.eu>
2026-02-26 14:45:22 +0100

    Move filter sensor interpolation to FilterSensorUnit class

--- src/ms_camera_model/filter_sensor/filter_sensor.py
+++ src/ms_camera_model/filter_sensor/filter_sensor.py
@@ -7,11 +7,13 @@
 '''
 
 from __future__ import annotations
-import numpy as np
-import pandas as pd
-from dataclasses import dataclass
 
 import logging
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+
 logger = logging.getLogger(__name__)
 
 
@@ -41,14 +43,17 @@ class FilterSensorUnit:
     sensor_spec: SensorSpecs | None = None
     combined_attenuation: np.ndarray | None = None
 
-    def load_filter_sensor(self, filename_filter: str, filename_sensor: str) -> None:
+    def load_filter_sensor(self, filename_filter: str,
+                           filename_sensor: str) -> None:
         """ Load filter transmission and sensor spectral sensitivity from a file
 
         :param filename_filter: Filename or path to the filter xlsx file
         :param filename_sensor: Filename or path to the sensor xlsx file
         """
 
-        logger.info(f"[FilterSensorUnit] Loading specifications\nFilter data: {filename_filter}\nSensor data: {filename_sensor}")
+        logger.info(
+            f"[FilterSensorUnit] Loading specifications\nFilter data: {filename_filter}\nSensor data: {filename_sensor}"
+        )
 
         filter_data = pd.read_excel(filename_filter)
         sensor_data = pd.read_excel(filename_sensor)
@@ -60,8 +65,41 @@ class FilterSensorUnit:
         """ Calculate combined attenuation of filter and sensor based on their transmission and qe (quantum efficiency) curve """
 
         logger.info("[FilterSensorUnit] Calculating combined attenuation")
-        logger.info(f"[FilterSensorUnit] Filter transmission: {self.filter_spec.filter_transmission.shape}, Sensor QE curve: {self.sensor_spec.sensor_qe_curve.shape}")
+        logger.info(
+            f"[FilterSensorUnit] Filter transmission: {self.filter_spec.filter_transmission.shape}, Sensor QE curve: {self.sensor_spec.sensor_qe_curve.shape}"
+        )
 
-        self.combined_attenuation = self.filter_spec.filter_transmission[:, 1] * self.sensor_spec.sensor_qe_curve[:, 1]
+        self.combined_attenuation = self.filter_spec.filter_transmission[:,
+                                                                         1] * self.sensor_spec.sensor_qe_curve[:,
+                                                                                                               1]
 
-        logger.info(f"[FilterSensorUnit] Combined attenuation: {self.combined_attenuation.shape}")
+        logger.info(
+            f"[FilterSensorUnit] Combined attenuation: {self.combined_attenuation.shape}"
+        )
+
+    def interpolate_to_hs_data(
+            self, hs_band_centers: list[float] | None) -> FilterSensorUnit:
+        """ Interpolate the provided filter and sensor data to hyperspectral data """
+
+        if hs_band_centers is None:
+            raise ValueError(f"Missing band center data for interpolation")
+
+        filter_interp = np.interp(hs_band_centers,
+                                  self.filter_spec.transmission[:, 0],
+                                  self.filter_spec.transmission[:, 1])
+        sensor_interp = np.interp(hs_band_centers,
+                                  self.sensor_spec.sensor_qe_curve[:, 0],
+                                  self.sensor_spec.sensor_qe_curve[:, 1])
+
+        interpolated_filter = FilterSpecs(
+            np.column_stack([hs_band_centers, filter_interp]),
+            self.filter_spec.name, self.filter_spec.supplier,
+            self.filter_spec.band_center, self.filter_spec.band_width)
+
+        interpolated_sensor = SensorSpecs(
+            np.column_stack([hs_band_centers,
+                             sensor_interp]), self.sensor_spec.name,
+            self.sensor_spec.supplier, self.sensor_spec.sensor_type)
+
+        return FilterSensorUnit(interpolated_filter, interpolated_sensor,
+                                np.array([]))
--- src/ms_camera_model/model/model.py
+++ src/ms_camera_model/model/model.py
@@ -8,16 +8,17 @@
 '''
 
 from __future__ import annotations
-import numpy as np
-
-from dataclasses import dataclass, field
 
 import logging
+from dataclasses import dataclass, field
+
+import numpy as np
+
 logger = logging.getLogger(__name__)
 
-from ms_camera_model.image_data import HyperspectralImageData, ImageData
-from ms_camera_model.filter_sensor import FilterSensorUnit, FilterSpecs, SensorSpecs
 from ms_camera_model.errors import NoImageData
+from ms_camera_model.filter_sensor import FilterSensorUnit, FilterSpecs, SensorSpecs
+from ms_camera_model.image_data import HyperspectralImageData, ImageData
 
 
 @dataclass
@@ -32,34 +33,42 @@ class MultispectralCameraModel:
     filter_sensor_units: list[FilterSensorUnit]
     out_data: ImageData = field(init=False)
 
-    def __post_init__(self) -> None:
+    def run_simulation(self) -> None:
         """ Match filter_sensor_units specs with hs_data band_centers and calculate out_data """
-        logger.info("[MSModel] Creating MultispectralCameraModel instance, proceeding with filter_sensor_units matching to provided hyperspectral data...")
+        logger.info(
+            "[MSModel] Creating MultispectralCameraModel instance, proceeding with filter_sensor_units matching to provided hyperspectral data..."
+        )
         self._filter_sensor_data_matching()
-        logger.info("[MSModel] Units matched. Proceeding with data extraction...")
+        logger.info(
+            "[MSModel] Units matched. Proceeding with data extraction...")
         self._extract_data()
-        logger.info("[MSModel] Simulated multispectral data extraction completed")
+        logger.info(
+            "[MSModel] Simulated multispectral data extraction completed")
 
     def _extract_data(self) -> None:
         """ Extract simulated multispectral data from the hyperspectral img_data using filter_sensor_units """
 
         if self.hs_data.img_data.size == 0:
             raise NoImageData
-        
-        self.hs_data.img_data = np.rot90(self.hs_data.img_data, k=1, axes=(0, 1))
 
         shape = self.hs_data.img_data.shape
-        modeled_ms_data = np.zeros((shape[0], shape[1], len(self.filter_sensor_units)))
+        modeled_ms_data = np.zeros(
+            (shape[0], shape[1], len(self.filter_sensor_units)))
         modeled_ms_data_band_centers = []
 
-        for i_unit in range(len(self.filter_sensor_units)):
-            self.filter_sensor_units[i_unit].calculate_combined_attenuation()
-            logger.info(f"[MSModel] FS_{i_unit} max combined attenuation is {self.filter_sensor_units[i_unit].combined_attenuation.max()}")
-            modeled_ms_data[:, :, i_unit] = self._calculate_band(self.filter_sensor_units[i_unit])
-            modeled_ms_data_band_centers.append(self.filter_sensor_units[i_unit].filter_spec.band_center)
+        for i_unit, unit in enumerate(self.filter_sensor_units):
+            unit.calculate_combined_attenuation()
+            logger.info(
+                f"[MSModel] FS_{i_unit} max combined attenuation is {unit.combined_attenuation.max()}"
+            )
+            modeled_ms_data[:, :, i_unit] = self._calculate_band(unit)
+            modeled_ms_data_band_centers.append(unit.filter_spec.band_center)
 
-        logger.info(f"[MSModel] modeled_ms_data max val: {modeled_ms_data.max()}")
-        self.out_data = ImageData(modeled_ms_data, modeled_ms_data_band_centers, len(self.filter_sensor_units))
+        logger.info(
+            f"[MSModel] modeled_ms_data max val: {modeled_ms_data.max()}")
+        self.out_data = ImageData(modeled_ms_data,
+                                  modeled_ms_data_band_centers,
+                                  len(self.filter_sensor_units))
 
     def _filter_sensor_data_matching(self) -> None:
         """ Interpolate filter transmission data to match bands from hyperspectral img data """
@@ -67,48 +76,17 @@ class MultispectralCameraModel:
         corrected_units = []
 
         for filter_sensor_unit in self.filter_sensor_units:
-            
-            filter_interp = np.interp(
-                    self.hs_data.band_centers,
-                    filter_sensor_unit.filter_spec.filter_transmission[:, 0],
-                    filter_sensor_unit.filter_spec.filter_transmission[:, 1])
-            sensor_interp = np.interp(
-                    self.hs_data.band_centers,
-                    filter_sensor_unit.sensor_spec.sensor_qe_curve[:, 0],
-                    filter_sensor_unit.sensor_spec.sensor_qe_curve[:, 1])
-
             corrected_units.append(
-                FilterSensorUnit(
-                    FilterSpecs(
-                        np.column_stack([
-                            self.hs_data.band_centers,
-                            filter_interp
-                        ]),
-                        filter_sensor_unit.filter_spec.name,
-                        filter_sensor_unit.filter_spec.supplier,
-                        filter_sensor_unit.filter_spec.band_center,
-                        filter_sensor_unit.filter_spec.band_width
-                    ),
-                    SensorSpecs(
-                        np.column_stack([
-                            self.hs_data.band_centers,
-                            sensor_interp
-                        ]),
-                        filter_sensor_unit.sensor_spec.name,
-                        filter_sensor_unit.sensor_spec.supplier,
-                        filter_sensor_unit.sensor_spec.sensor_type
-                    ),
-                    np.array([])
-                )
-            )
+                filter_sensor_unit.interpolate_to_hs_data(
+                    self.hs_data.band_centers))
 
-        self.filter_sensor_units = corrected_units
-
-
-    def _calculate_band(self, filter_sensor_unit: FilterSensorUnit) -> np.ndarray:
+    def _calculate_band(self,
+                        filter_sensor_unit: FilterSensorUnit) -> np.ndarray:
         """ Calculate the image created by a filter sensor unit """
 
-        logger.info("[MSModel] Calculating single band image from hyperspectral data...")
+        logger.info(
+            "[MSModel] Calculating single band image from hyperspectral data..."
+        )
 
         data_through_unit = self.hs_data.img_data * filter_sensor_unit.combined_attenuation
         logger.info(f"[MSModel] Max data val: {data_through_unit.max()}")
